567df2bc9f6db022bb7b27fb49b2d66c856da4b0
mark <markqc@users.noreply.github.com>
2021-01-18 18:40:41 +0800

    Update helloworldjavatest.java

--- helloworldjavatest.java
+++ helloworldjavatest.java
@@ -3,5 +3,6 @@ public class helloworldjavatest{
 	public static void main(String[] args){
 		
 		System.out.println("Hello World again from GitHub - HI Testing POLL SCM - 2nd change test webhook xxxx chagnes sss");
+		System.out.println("TeamCity Test");
 	}
 }
